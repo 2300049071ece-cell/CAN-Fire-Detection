9a10aae2fc8779878de1e52b70c709a33eb57060
pardhu-embedded <2300049071ece@gmail.com>
2025-12-03 12:42:21 +0530

    Create system_config.h

---
+++ Core/Inc/Core/Inc/Core/Inc/system_config.h
@@ -0,0 +1,11 @@
+#ifndef SYSTEM_CONFIG_H
+#define SYSTEM_CONFIG_H
+
+#include "stm32f10x.h"
+
+/* SystemInit() is usually provided by CMSIS or STM32 startup code.
+   Including this header keeps the project modular and clean. */
+
+void SystemInit(void);
+
+#endif // SYSTEM_CONFIG_H
